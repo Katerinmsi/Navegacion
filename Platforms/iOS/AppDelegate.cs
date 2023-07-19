afcd44b8b13e57abe5595f7065b69640f81fd005
MINEDUCYT <MINEDUCYT@TATIANA>
2023-07-19 08:58:09 -0600

    Agregar archivos de proyecto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Navegacion;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
